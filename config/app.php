2bd8a43b6a6b54b8dfa3bf6d82f56348338e14c3
Fabien Lecouve <fabien.lecouve@hotmail.com>
2026-05-02 09:54:15 +0200

    fix: adding fr_FR for french factories

--- config/app.php
+++ config/app.php
@@ -82,7 +82,7 @@ return [
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'fr'),
 
-    'faker_locale' => env('APP_FAKER_LOCALE', 'fr'),
+    'faker_locale' => env('APP_FAKER_LOCALE', 'fr_FR'),
 
     /*
     |--------------------------------------------------------------------------
